7089f9d021d868e9145c7824a3e26ebd7cbd5378
AnjanaLP <anjana.lpn@gmail.com>
2022-01-18 20:54:08 +0000

    player can choose a weapon

--- spec/features/play_game_spec.rb
+++ spec/features/play_game_spec.rb
@@ -1,8 +1,14 @@
 feature 'Play the game' do
+  before { register_name }
+
   scenario 'see the weapon choices' do
-    register_name
     expect(page).to have_button "Rock"
     expect(page).to have_button "Paper"
     expect(page).to have_button "Scissors"
+  end
+
+  scenario 'player chooses a weapon' do
+    click_button "Paper"
+    expect(page).to have_content "You chose Paper!"
   end
 end
